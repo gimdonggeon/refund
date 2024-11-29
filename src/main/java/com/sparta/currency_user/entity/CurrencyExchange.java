18395c4e447cbbf0f0598e6259493e9855dc7fb1
popcon1234 <popcon1234@naver.com>
2024-11-29 10:42:40 +0900

    feat : 중간엔티티 작성
    Body : Currency엔티티와 User엔티티를 연결해주는 CurrencyExchange라는 중간 엔티티 작성

---
+++ src/main/java/com/sparta/currency_user/entity/CurrencyExchange.java
@@ -0,0 +1,38 @@
+package com.sparta.currency_user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyExchange {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+    private Long toCurrencyId;
+    private BigDecimal amountBeforeExchangeKrw;
+    private BigDecimal amountAfterExchange;
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    private Currency toCurrency;
+
+    public void cancel() {
+        this.status = "cancelled";
+    }
+
+    public void calculateAmountAfterExchange(BigDecimal exchangeRate) {
+        this.amountAfterExchange = getAmountBeforeExchangeKrw().divide(exchangeRate, 2, RoundingMode.HALF_UP);
+    }
+}
